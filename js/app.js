1b2e2b4348c7d2412e934d1766e378c30a1f7c99
Amany Siraj <amanysirajaldin@gmail.com>
2017-08-05 11:41:12 +0200

    Add cat's Nicknames using control flow

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@ var ViewModel = function () {
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttr = ko.observable('');
+     // Add Nicknames for the cat
+    this.nicknames = ['Tabtab', 'T-Bone', 'Mr.T'];
     // the peices are functionally separate
     // even though the model is defined inside the ViewModel
     // the functionality is separate
@@ -18,17 +20,18 @@ var ViewModel = function () {
     // we don't need the other get snd set functions
     // because KO will handle the view to model
     // and model to view sync for us
-    console.log(this.clickCount());
+
     // Add cat levels 
     // using computed observable
     this.catLevel = ko.computed(function (){
-        //console.dir(this.clickCount);
-        if(this.clickCount() < 10){
+        if(this.clickCount < 10){
             return "New Born";
-        }else if(this.clickCount() >= 10){
+        }else if(this.clickCount >= 10){
             return "Infant";
         }
     }, this);
+    
+   
 
 
 }
