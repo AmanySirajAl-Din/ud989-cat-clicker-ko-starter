9abf2892a524ac7f1e436eb8bde92cdf46f58b3c
Amany Siraj <amanysirajaldin@gmail.com>
2017-08-05 12:07:42 +0200

    remove prev commets from app.js

--- js/app.js
+++ js/app.js
@@ -2,19 +2,13 @@
 // to be able to create a new cat 
 // without repeating the same code
 var newCat = function(){
-    // add my model data here
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
     this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
     this.imgAttr = ko.observable('');
-    // Add Nicknames for the cat
     this.nicknames = ['Tabtab', 'T-Bone', 'Mr.T'];
-    // the peices are functionally separate
-    // even though the model is defined inside the ViewModel
-    // the functionality is separate
+    
 
-    // Add cat levels 
-    // using computed observable
     this.catLevel = ko.computed(function () {
         var numOfClicks = this.clickCount();
         if (numOfClicks < 10) {
@@ -35,16 +29,9 @@ var newCat = function(){
 
 var ViewModel = function () {
     
-    // then we need the function to increase my counter
     this.incrementCounter = function () {
         this.clickCount(this.clickCount() + 1);
     };
-    // the only time we need to write ViewModel methods are 
-    // when we need to actually change something ourselves
-    // we don't need the other get snd set functions
-    // because KO will handle the view to model
-    // and model to view sync for us
-
 }
 
 ko.applyBindings(new ViewModel);
