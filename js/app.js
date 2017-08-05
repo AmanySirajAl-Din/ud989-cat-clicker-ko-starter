6453a0028c1a5a8a22e1dbd34d29e582b44d2989
Amany Siraj <amanysirajaldin@gmail.com>
2017-08-05 09:05:18 +0200

    Add clickCount ViewModel method

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var ViewModel = function (){
+var ViewModel = function () {
     // add my model data here
     this.clickCount = ko.observable(0);
     this.name = ko.observable('Tabby');
@@ -7,12 +7,19 @@ var ViewModel = function (){
     // the peices are functionally separate
     // even though the model is defined inside the ViewModel
     // the functionality is separate
-    
-    
 
-    
-    
-    
+
+    // then we need the function to increase my counter
+    this.incrementCounter = function () {
+        this.clickCount(this.clickCount() + 1);
+    };
+    // the only time we need to write ViewModel methods are 
+    // when we need to actually change something ourselves
+    // we don't need the other get snd set functions
+    // because KO will handle the view to model
+    // and model to view sync for us
+
+
 }
 
 ko.applyBindings(new ViewModel);
