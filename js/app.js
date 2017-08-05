7d51eeaca3ae1cf2181187bdf64e8eb429240438
Amany Siraj <amanysirajaldin@gmail.com>
2017-08-05 08:59:07 +0200

    Add model data inside ViewModel

--- js/app.js
+++ js/app.js
@@ -1,4 +1,17 @@
 var ViewModel = function (){
+    // add my model data here
+    this.clickCount = ko.observable(0);
+    this.name = ko.observable('Tabby');
+    this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+    this.imgAttr = ko.observable('');
+    // the peices are functionally separate
+    // even though the model is defined inside the ViewModel
+    // the functionality is separate
+    
+    
+
+    
+    
     
 }
 
